58785f93eb17bf927b81e1ed88eefc0b39776f3a
Sergio Navarro Pino <sergio.navarro@analyticalways.com>
2017-06-29 13:03:09 +0200

    Added client that consumes nuget

---
+++ src/CommonTools/CoreClient/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using AnalyticAlways.CommonToolsCore;
+
+namespace CoreClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Validando WTF! as a regular expression!");
+            var isValid = RegexUtil.IsRegexPatternValid("WTF!");
+            Console.WriteLine($"Is Valid={isValid}");
+        }
+    }
+}
