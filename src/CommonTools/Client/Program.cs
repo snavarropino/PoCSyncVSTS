86c7807261f03b8f3bc54bd7ae2d73a2a43ee78d
Sergio Navarro Pino <sergio.navarro@analyticalways.com>
2017-06-29 17:18:45 +0200

    Added full framework client

---
+++ src/CommonTools/Client/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using AnalyticAlways.CommonTools;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Validando WTF! as a regular expression!");
+            var isValid = RegexUtil.IsRegexPatternValid("WTF!");
+            Console.WriteLine($"Is Valid={isValid}");
+        }
+    }
+}
